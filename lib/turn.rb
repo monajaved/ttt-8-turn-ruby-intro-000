7e9603e12e57fe705f2e530faa2de0867c658382
Mona Javed Syed <monajaved93@gmail.com>
2020-10-18 07:43:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,8 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  # valid_move?(board, index)
+  if valid_move?(board, index) == true
+    return true
 
 
 end
