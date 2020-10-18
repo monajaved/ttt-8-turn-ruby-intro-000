1dbc21a0f0e5e5a23f336abaff708e04842d3425
Mona Javed Syed <monajaved93@gmail.com>
2020-10-18 07:07:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,13 @@ def valid_move?(board, index)
     return true
   else
     return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
